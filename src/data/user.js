af5654b07e14faa4e752741a452d7a8a4d76c73d
Victor <victormugo08@gmail.com>
2025-04-13 11:58:33 +0300

    modified user.js

--- src/data/user.js
+++ src/data/user.js
@@ -1,7 +1,6 @@
-const username = "Liza";
-const city = "New York";
+const username = "Veektor";
+const city = "Nairobi";
 
 const image = "https://i.imgur.com/mV8PQxj.gif";
 
-// export all three variables as named exports
 export { username, city, image};
